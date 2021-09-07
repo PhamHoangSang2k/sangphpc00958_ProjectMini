e0f179d03f138c5a080649e6b9b296c91ebd436c
PhamHoangSang2k <90090254+PhamHoangSang2k@users.noreply.github.com>
2021-09-07 16:47:22 +0700

    Delete sangphpc00958_ProjectMini directory

--- sangphpc00958_ProjectMini/src/main/java/sangphpc00958_ProjectMini/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package sangphpc00958_ProjectMini.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sangphpc00958_ProjectMini.entity.Category;
-
-public interface CategoryDao extends JpaRepository<Category, String>{
-
-}
